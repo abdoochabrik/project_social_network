a17e08fdb86dd836b7873140fb22db4562e07ada
Dell <abdoo.chbrik@gmail.com>
2021-05-05 01:07:19 +0200

    home page finished

---
+++ src/Data.js
@@ -0,0 +1,60 @@
+export const Users = [
+    {
+        id: 1,
+        profilePicture: "assets/person/1.jpeg",
+        username: "chabrik abdellatif"
+    },
+
+    {
+        id: 2,
+        profilePicture: "assets/person/2.jpeg",
+        username: "ya ssi ne"
+    },
+    
+    {
+        id: 3,
+        profilePicture: "assets/person/3.jpeg",
+        username: "zairi jalal"
+    },
+    {
+        id: 4,
+        profilePicture: "assets/person/4.jpeg",
+        username: "ibraaa hima"
+    }
+    
+];
+
+export const Posts = [
+    {
+        id: 1,
+        desc: "hi i'm chabrik",
+        photo: "assets/post/1.jpeg",
+        userId: 1,
+        like: 32,
+        Comment: 9 
+    },
+    {
+        id: 2,
+        desc: "hi i'm jalal",
+        photo: "assets/post/2.jpeg",
+        userId: 3,
+        like: 65,
+        Comment: 6 
+    },
+    {
+        id: 3,
+        desc: "hi i'm yassine",
+        photo: "assets/post/3.jpeg",
+        userId: 2,
+        like: 12,
+        Comment: 6 
+    },
+    {
+        id: 4,
+        desc: "hi i'm ibrahima",
+        photo: "assets/post/4.jpeg",
+        userId: 4,
+        like: 52,
+        Comment: 15 
+    }
+];
