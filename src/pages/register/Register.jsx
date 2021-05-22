1773df573fb560ec65a9a9f04917c22e636c7240
echyassine <yassinekaist97@gmail.com>
2021-05-22 11:00:46 +0100

    register a user

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,6 +1,34 @@
 import './register.css'
+import {useState} from 'react'
 
-export default function Register() {
+export default function Register({addUser}) {
+    const [user, setUser] = useState({
+        "username":'',     
+        "email":'',        
+        "password":'',     
+        "passwordAgain":'',
+    })
+
+    const handleChange = e =>{
+        setUser({...user, [e.target.name]: e.target.value})
+    }
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        
+    const { password, passwordAgain } = user;
+    if (password !== passwordAgain) {
+        alert("Passwords don't match");
+    } else {
+        addUser(user)
+        setUser({
+            "username":'',     
+            "email":'',        
+            "password":'',     
+            "passwordAgain":''
+            })
+        }
+    }
+
    return (
       <div className="login">
           <div className="loginWrapper">
@@ -10,17 +38,37 @@ export default function Register() {
                         Connect with your friends on CCEZ SOCIAL.
                     </span>
               </div>
-
+            <form action="" method="post"  >
               <div className="loginRight">
                     <div className="loginBox">
-                        <input placeholder="username" className="loginInput"/>
-                        <input placeholder="Email" className="loginInput"/>
-                        <input placeholder="password" className="loginPassword"/>
-                        <input placeholder="passwordAgain" className="loginPassword"/>
-                        <button className="loginButton">Register Now</button>
+                        <input onChange={handleChange}
+                            placeholder="username" 
+                            name="username" 
+                            type="text" 
+                            className="loginInput" 
+                             />
+                        <input onChange={handleChange} 
+                            placeholder="Email" 
+                            name="email" 
+                            type="email" 
+                            className="loginInput"        />
+                        <input onChange={handleChange}
+                            placeholder="password" 
+                            name="password" 
+                            type="password" 
+                            className="loginPassword"  />
+                        <input onChange={handleChange}
+                            placeholder="confirm password" 
+                            name="passwordAgain"  
+                            type="password" 
+                            className="loginPassword"  />
+                        <button type="submit" 
+                        onClick={handleSubmit} 
+                        className="loginButton">Register Now</button>
                         <button className="loginCreate">Login Into Your Account</button>
                     </div>
               </div>
+            </form> 
           </div>
       </div>
    );
